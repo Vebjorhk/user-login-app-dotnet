66ea05ed5f6b753866e1125b49aa362ab3ad1a1e
Vebjørn Haug Kåsene <77414086+Vebjorhk@users.noreply.github.com>
2025-11-20 15:02:03 +0100

    add tailwind support

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,47 +1,20 @@
 import { useState, useEffect} from 'react'
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import viteIcon from '/vite.svg'
+import reactIcon from './assets/react.svg'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  // 👇 Add this useEffect block
-  useEffect(() => {
-  fetch("/api/message") // <-- relative URL so Vite proxy kicks in
-    .then((res) => res.json())
-    .then((data) => {
-      console.log("API response:", data);
-    })
-    .catch((err) => {
-      console.error("Error fetching API:", err);
-    });
-}, []);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-brand">
+        <div className="flex gap-15">
+          <img src={viteIcon} alt="Vite Icon" className="w-30 h-30" />
+          <img src={reactIcon} alt="React Icon" className="w-30 h-30" />
+        </div>
+        <h1 className="text-4xl font-bold text-white mt-6">
+          Hello Tailwind v4 👋
+        </h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
